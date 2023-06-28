81b976b36d560446b1c8723d742d0892bd70a39d
Greg Lixandrão <contato@greglixandrao.com>
2023-06-27 21:35:00 -0300

    adding user_data to main.tf file

--- main.tf
+++ main.tf
@@ -17,8 +17,14 @@ resource "aws_instance" "app_server" {
   ami = "ami-03f65b8614a860c29"
   instance_type = "t2.micro"
   key_name = "aws-estudos-user-greglixandrao"
+#   user_data = <<-EOF
+# 				#!/bin/bash
+# 				cd /home/ubuntu
+# 				echo "<h1>Hello World done with Terraform and AWS</h1>" > index.html
+# 				nohup busybox httpd -f -p 8080 &
+# 				EOF
   tags = {
-		Name = "EstudoAluraIaC"
+		Name = "EstudoAluraIaC com webserver"
 		curso = "AluraIaC"
 	}
 }
