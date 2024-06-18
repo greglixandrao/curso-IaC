c1b7dcfb4a69de3e9f45c1e1cfb18262a44d729a
Greg Lixandrão <contato@greglixandrao.com>
2024-06-18 08:26:34 -0300

    update aws provider version and ssm profile

--- main.tf
+++ main.tf
@@ -1,30 +1,31 @@
 terraform {
   required_providers {
-	aws = {
-		source = "hashicorp/aws"
-		version = "~> 3.27"
-	}
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
-  required_version = ">= 0.14.9"
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  profile = "aws-estudos"
-  region = "us-west-2"
+  profile = "default"
+  region  = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami = "ami-03f65b8614a860c29"
-  instance_type = "t2.micro"
-  key_name = "aws-estudos-user-greglixandrao"
-#   user_data = <<-EOF
-# 				#!/bin/bash
-# 				cd /home/ubuntu
-# 				echo "<h1>Hello World done with Terraform and AWS</h1>" > index.html
-# 				nohup busybox httpd -f -p 8080 &
-# 				EOF
+  ami                  = "ami-03c983f9003cb9cd1"
+  instance_type        = "t3.micro"
+  key_name             = "aws-alura-api-go-dev"
+  iam_instance_profile = "EC2InstanceRole"
+  #   user_data = <<-EOF
+  # 				#!/bin/bash
+  # 				cd /home/ubuntu
+  # 				echo "<h1>Hello World done with Terraform and AWS</h1>" > index.html
+  # 				nohup busybox httpd -f -p 8080 &
+  # 				EOF
   tags = {
-		Name = "EstudoAluraIaC com webserver"
-		curso = "AluraIaC"
-	}
+    Name  = "EstudoAluraIaC com webserver"
+    curso = "AluraIaC"
+  }
 }
