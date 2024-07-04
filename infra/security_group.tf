be2369ea28945a78d2740d219bcb0a56ba482e89
Greg Lixandrão <contato@greglixandrao.com>
2024-07-04 07:46:03 -0300

    fix security group name with variables

--- infra/security_group.tf
+++ infra/security_group.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "general_access" {
-  name = "var.securityGroup"
+  name = var.securityGroup
   ingress {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
