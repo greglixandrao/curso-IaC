fc6f37d14cdff8f04de17034252e976ee7614596
Greg Lixandrão <contato@greglixandrao.com>
2024-07-05 09:45:13 -0300

    Dev. change instance type and maximo. Add condiction

--- env/Dev/main.tf
+++ env/Dev/main.tf
@@ -1,10 +1,11 @@
 module "dev-environment" {
   source        = "../../infra"
-  instance-type = "t3.micro"
+  instance-type = "t2.micro"
   aws-region    = "us-west-2"
   ssh-key       = "iac-dev"
   securityGroup = "Dev"
   minimum       = 0
-  maximo        = 2
+  maximo        = 1
   asGroup       = "Dev"
+  producao      = false
 }
